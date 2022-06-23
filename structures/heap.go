d8ba57b5105678cc9526d4f188d5f2e7ed9c4063
devlipe <felipe.p.ferreira@ufv.br>
2022-06-23 14:47:40 -0300

    Add Heap

---
+++ structures/heap.go
@@ -0,0 +1,111 @@
+// Package heap provides heap operations for any type that implements
+// heap.Interface. A heap is a tree with the property that each node is the
+// minimum-valued node in its subtree.
+//
+// The minimum element in the tree is the root, at index 0.
+
+package structures
+
+import "sort"
+
+// The Interface type describes the requirements
+// for a type using the routines in this package.
+// Any type that implements it may be used as a
+// min-heap with the following invariants (established after
+// Init has been called or if the data is empty or sorted):
+//
+//	!h.Less(j, i) for 0 <= i < h.Len() and 2*i+1 <= j <= 2*i+2 and j < h.Len()
+//
+// Note that Push and Pop in this interface are for package heap's
+// implementation to call. To add and remove things from the heap,
+// use heap.Push and heap.Pop.
+type Interface interface {
+	sort.Interface
+	Push(x any) // Add x as elem Len()
+	Pop() any   // Remove and return elem Len() -1
+}
+
+// Init establishes the heap invariants required by the other routines in this package.
+// Init is idempotent with respect to the heap invariants
+// and may be called whenever the heap invariants may have been invalidated.
+// The complexity is O(n) where n = h.Len().
+func Init(h Interface) {
+	//heapify
+
+	n := h.Len()
+	for i := n/2 - 1; i >= 0; i-- {
+		down(h, i, n)
+	}
+}
+
+// Push pushes the element x onto the heap.
+// The complexity is O(log n) where n = h.Len().
+func Push(h Interface, x any) {
+	h.Push(x)
+	up(h, h.Len()-1)
+}
+
+// Pop removes and returns the minimum element (according to Less) from the heap.
+// The complexity is O(log n) where n = h.Len().
+// Pop is equivalent to Remove(h, 0).
+func Pop(h Interface) any {
+	n := h.Len() - 1
+	h.Swap(0, n)  // Get last element and put it at the top
+	down(h, 0, n) // Restore heap invariant from position 0
+	return h.Pop()
+}
+
+// Remove removes and returns the element at index i from the heap.
+// The complexity is O(log n) where n = h.Len().
+func Remove(h Interface, i int) any {
+	n := h.Len() - 1
+	if n != i {
+		h.Swap(i, n)
+		if !down(h, i, n) {
+			up(h, i)
+		}
+	}
+	return h.Pop()
+}
+
+// Fix re-establishes the heap ordering after the element at index i has changed its value.
+// Changing the value of the element at index i and then calling Fix is equivalent to,
+// but less expensive than, calling Remove(h, i) followed by a Push of the new value.
+// The complexity is O(log n) where n = h.Len().
+func Fix(h Interface, i int) {
+	if !down(h, i, h.Len()) {
+		up(h, i)
+	}
+}
+
+func up(h Interface, j int) {
+	for {
+		parent := (j - 1) / 2
+
+		if parent == j || !h.Less(j, parent) {
+			break
+		}
+		h.Swap(j, parent)
+		j = parent
+	}
+}
+
+func down(h Interface, i0, n int) bool {
+	i := i0
+	for {
+		smallerSon := 2*i + 1
+		if smallerSon >= n || smallerSon < 0 { // overflow
+			break
+		}
+		leftChild := smallerSon
+		if rightChild := smallerSon + 1; rightChild < n && h.Less(rightChild, leftChild) {
+			smallerSon = rightChild
+		}
+		if h.Less(smallerSon, i) {
+			break
+		}
+		h.Swap(i, smallerSon)
+		i = smallerSon
+	}
+	return i > i0
+}
